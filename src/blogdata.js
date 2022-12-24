912834cd83d54bf8445b21811c749f3ece59d943
Camilo <camiloarboleda821@gmail.com>
2022-12-24 08:34:01 -0500

    a

---
+++ src/blogdata.js
@@ -0,0 +1,16 @@
+const blogdata = [];
+blogdata.push({
+    title: '¿Que es React?',
+    slug: 'que-es-react',
+    content: 'React es el mejor framework de javascript',
+    author: 'Camilo'
+})
+blogdata.push({
+    title: '¿Que es Angular?',
+    slug: 'que-es-angular',
+    content: 'Angular es el mejor framework de javascript',
+    author: 'Camilo'
+})
+
+
+export { blogdata }
